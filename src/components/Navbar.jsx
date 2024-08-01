fbdaac1acba5376867e392855ae1255e4b3cea3d
Joao Donaton <joao.vitor.donaton@gmail.com>
2024-08-01 20:40:47 -0300

    initial

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return  <nav>
+        <ul>
+            <li>BabelPages</li>
+            <li></li>
+            <li>Login</li>
+        </ul>
+    </nav>
+}
+
+export default Navbar;
